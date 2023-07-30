cdba8a9a6fb47f4d50e572ff1054e5cb08733aea
Milena <mi_lky@mail.ru>
2023-07-30 09:30:03 +0300

    [init]

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Административная панель',
+  description: 'Панель управления для администратора',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ru">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
